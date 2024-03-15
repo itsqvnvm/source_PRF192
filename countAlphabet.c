493397129fa48ad1652df572d2ef33b3c4aa9c16
vinhdaoquangg <qvd.personal@icloud.com>
2024-03-15 22:13:34 +0700

    add countAlphabet.c

---
+++ countAlphabet.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define ALPHABET_SIZE 26
+
+// void countAlphabets(const char str[], int count[]) {
+//     int i = 0;
+//     while (str[i]) {
+//         if (isalpha(str[i])) {
+//             int index = tolower(str[i]) - 'a';
+//             count[index]++;
+//         }
+//         i++;
+//     }
+// }
+
+void countAlphabets(const char *str, int *count) {
+    while (*str) {
+        if (isalpha(*str)) {
+            int index = tolower(*str) - 'a';
+            count[index]++;
+        }
+        str++;
+    }
+}
+
+int main() {
+    char str[100];
+    int count[ALPHABET_SIZE] = {0};
+    int totalCount = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    countAlphabets(str, count);
+
+    for (int i = 0; i < ALPHABET_SIZE; i++) {
+        totalCount += count[i];
+    }
+
+    printf("Total Alphabet Count: %d\n", totalCount);
+
+    return 0;
+}
