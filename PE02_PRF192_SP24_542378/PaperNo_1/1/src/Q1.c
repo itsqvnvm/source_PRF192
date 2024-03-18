e84bc038ba6276d33ee1b379ebc4829f419c208c
vinhdaoquangg <qvd.personal@icloud.com>
2024-03-18 14:04:54 +0700

    Add 2 folder of first day

---
+++ PE02_PRF192_SP24_542378/PaperNo_1/1/src/Q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//------------------------------------------
+void printAroundArea(int radius, int height ) {
+	//Write your statements here
+    double area = 2 * M_PI * radius * (radius + height);
+    printf("%.2f\n", area);
+	//End your statements
+}
+//=============DO NOT ADD NEW OR CHANGE STATEMENTS IN THE MAIN FUNCTION========
+int main() {
+	system("cls");
+	printf("\nTEST Q1 (2 marks):\n");
+	int r, h;	
+	do {
+		printf("Enter radius and height of cylinder: ");
+		scanf("%d%d",&r, &h);
+	} while((r<=0) || (h<=0));	
+	printf("\nOUTPUT:\n");
+	printAroundArea(r, h);
+	printf("\n");
+	system ("pause");
+	return(0);
+}
+//============================================================
